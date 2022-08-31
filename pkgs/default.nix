b9dd89a46bd92429c18ba5aded44f4c0a12a1b53
skarlett <skarlett@github.com>
2022-08-31 14:52:38 -0500

    add "skarlett.hello" (experiment)

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,13 +5,17 @@ let
     echo "Hello from the skarlett channel!"
   '';
 
+  skarlett.hello = pkgs.writeShellScriptBin "hello" ''
+    echo "Hello from the skarlett channel!"
+  '';
+
   transfer = pkgs.callPackage ../dotfiles;
   coggiebot = pkgs.callPackage ../coggiebot;
 
   pkgs = import sources.nixpkgs {
     overlays = [
       (self: super: {
-        inherit skarletthello transfer coggiebot;
+        inherit skarlett skarletthello transfer coggiebot;
       })
     ];
   };
