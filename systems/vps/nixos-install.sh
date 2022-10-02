3d489b87109fbd1e0922b709747d087bee6206de
skarlett <skarlett@github.com>
2022-10-02 07:48:45 -0500

    add vps conf

---
+++ systems/vps/nixos-install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -ex
+
+parted --script /dev/vda mklabel msdos
+parted --script /dev/vda mkpart primary 1MB 100%
+parted --script /dev/vda set 1 boot on
+mkfs.ext4 /dev/vda1 -L ROOT
+
+mount /dev/vda1 /mnt
+
+nixos-generate-config --root /mnt
+
+mv configuration.nix /mnt/etc/nixos/configuration.nix
